fcfa4eb9299f7ab5c9215602c0737bec5439191c
Andrey Morales <andreymorj05@gmail.com>
2026-06-01 01:07:11 -0600

    Update interfaz_inicial.py
    Se añadió la interfaz base, necesita mejoras

--- interfaz_inicial.py
+++ interfaz_inicial.py
@@ -1,1 +1,27 @@
+from easygui import *
+import easygui
+#import life_like_gui
+import sys
+
+
+ret_val = msgbox("Bienvenido al programa principal, proximamente escoja su opcion preferida!")
+if ret_val is None: 
+    sys.exit(0)
+
+msg ="Escoja la opción preferida?\nOr Press <cancel> to exit."
+title = "Conway y Hormiga de Langton"
+choices = ["Conway", "Langton Ant"]
+hello = 0
+while 1:
+    choice = choicebox(msg, title, choices)
+    if choice is None:
+        sys.exit(0)
+    if choice == "Conway":
+        new_choice = enterbox("Diga sus reglas (Ejemplo: 1)")      
+        if new_choice is None:
+            continue    
+        if new_choice == "1":
+    if choice == "Hormiga de Langton":
+        
+        msgbox("Escogiste: {}".format(choice))
 
