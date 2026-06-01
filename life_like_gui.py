7ac48486c9e3ce92746ce5120330c5f52aee1775
Andrey Morales <andreymorj05@gmail.com>
2026-05-31 23:36:08 -0600

    Update life_like_gui.py
    se importa la gui anteriormente trabajada en clase

--- life_like_gui.py
+++ life_like_gui.py
@@ -1,1 +1,47 @@
+import pygame
+import conway_logica as con
 
+tam = 10
+filas = 50
+columnas = 50
+tick = 10
+
+def main():
+    pygame.init()
+    clock = pygame.time.Clock()
+    M = con.generar_matriz(filas, columnas)
+    w, h = columnas * tam, filas * tam
+    window = pygame.display.set_mode((w, h))
+    loop = True
+    pausa = False
+    while loop:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                loop = False
+            if event.type == pygame.KEYDOWN:
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_p]:
+                    pausa = not pausa
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                buttons = pygame.mouse.get_pressed()
+                x, y = pygame.mouse.get_pos()
+                if buttons[0]:
+                    f = y // tam
+                    c = x // tam
+                    M[f][c] = (M[f][c] + 1) % 2
+                    
+        window.fill((0, 0, 0))
+        for f in range(filas):
+            for c in range(columnas):
+                if M[f][c] == 1:
+                    x = c * tam
+                    y = f * tam
+                    pygame.draw.rect(window, (0, 255, 128), (x, y, tam, tam))
+        if not pausa:
+            M = con.transicion(M)
+        pygame.display.update()
+        clock.tick(10)
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
