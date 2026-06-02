e6c14c1689e888cc3ff36165d3bf6a299d4a44e5
Andrey Morales <andreymorj05@gmail.com>
2026-06-02 12:55:10 -0600

    Update interfaz_inicial.py
    Se añade la interfaz principal queda pendiente la revision de restricciones

--- interfaz_inicial.py
+++ interfaz_inicial.py
@@ -1,27 +1,41 @@
 from easygui import *
 import easygui
-#import life_like_gui
-import sys
 
+def ini_int():
+    #Variables encargadas de dar los mensajes y guardar valores:
+    mensaje_inicial = "¡Hola Usuario! Bienvenido al programa del Juego de la Vida de Conway y La Hormiga de Langton."
+    decision = "¿Desea usted continuar?"
+    mensaje_despedida = "¡Hasta pronto, tenga bonito día!"
+    decisiones = ["CONWAY","LANGTON"]
+    titulo = "PROGRAMA PRINCIPAL"
+    reglas = 0
+    filas = 0
+    cols = 0
+    celdas = 0
+    mensaje_pedida = "Introduzca las reglas,filas,columnas y el tamaño:"
+    titulo_pedida = "DATOS NECESARIOS"
+    nombres_de_datos = ["Reglas","Filas","Columnas","Tamaño de celdas"]
+    valor_de_datos = []
 
-ret_val = msgbox("Bienvenido al programa principal, proximamente escoja su opcion preferida!")
-if ret_val is None: 
-    sys.exit(0)
+    #Interfaz principal:
 
-msg ="Escoja la opción preferida?\nOr Press <cancel> to exit."
-title = "Conway y Hormiga de Langton"
-choices = ["Conway", "Langton Ant"]
-hello = 0
-while 1:
-    choice = choicebox(msg, title, choices)
-    if choice is None:
-        sys.exit(0)
-    if choice == "Conway":
-        new_choice = enterbox("Diga sus reglas (Ejemplo: 1)")      
-        if new_choice is None:
-            continue    
-        if new_choice == "1":
-    if choice == "Hormiga de Langton":
-        
-        msgbox("Escogiste: {}".format(choice))
-
+    if ccbox(mensaje_inicial,choices=("CONTINUAR", "SALIR")) == True:   
+        programas =buttonbox("Escoja que desea hacer:",titulo,decisiones)
+        if programas == "CONWAY":
+            msgbox("Escogiste Conway")
+            valor_de_datos = multenterbox(mensaje_pedida,titulo_pedida, nombres_de_datos)
+            if valor_de_datos == None:
+                msgbox(mensaje_despedida)
+            elif valor_de_datos[0] == "":
+                msgbox("La lista está vacia, por favor introduzca los datos")
+        elif programas == "LANGTON":
+            msgbox("Escogiste Langton")
+            valor_de_datos = multenterbox(mensaje_pedida,titulo_pedida, nombres_de_datos)
+            if valor_de_datos == None:
+                msgbox(mensaje_despedida)
+            elif valor_de_datos[0] == "":
+                msgbox("La lista está vacia, por favor introduzca los datos")
+        else:
+            msgbox(mensaje_despedida)
+    else:
+        msgbox(mensaje_despedida)
