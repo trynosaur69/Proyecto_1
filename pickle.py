22a1506432c3700547a175a9ea616c136b65dc80
AlexartG <alexart6122001@gmail.com>
2026-06-03 15:31:34 -0600

    Update pickle.py
    caca

--- pickle.py
+++ pickle.py
@@ -1,1 +1,27 @@
+import colorsys
+import pickle
 
+#Gradientes
+#hsv_to_rgb
+h = 0
+S = 1
+V = 1
+# i / 6
+#rgb = hsv_to_rgb(_, 1, 1):
+
+
+
+
+#pickle
+#para escribir
+archivo = open("datos.hormiga", "wb")
+#lista
+#diccionario ----> d
+#matriz
+#variables  #donde  #qué se guarda
+pickle.dump(archivo, d)
+
+#Para leer
+archivo = open("datos.hormiga", "rb")
+d = pickle.load(archivo)
+archivo.close()
