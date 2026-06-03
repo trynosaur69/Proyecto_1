9e7ff977d51c7c87783dd14ab1e5338b6044dec0
AlexartG <alexart6122001@gmail.com>
2026-06-03 12:35:13 -0600

    Update langton_ant_gui.py
    Pulido

--- langton_ant_gui.py
+++ langton_ant_gui.py
@@ -1,8 +1,8 @@
 import pygame
 import Hormiga_de_Langton_Lógica as Horlan
 
-TAM = 10
-TICK = 1
+TAM = 3
+TICK = 100000
 
 def main():
     pygame.init()
@@ -12,20 +12,18 @@ def main():
     window = pygame.display.set_mode((ancho, alto))
     clock = pygame.time.Clock()
     loop = True
+    window.fill(Horlan.color[0])
     while loop:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 loop = False
-        window.fill((0, 0, 0))
         for f in range(Horlan.filas):
             for c in range(Horlan.cols):
                 x = c * TAM
                 y = f * TAM
-                if Horlan.matriz[f][c] == 0:
-                    pygame.draw.rect(window, (0, 0, 0), (x, y, TAM, TAM))
-                elif Horlan.matriz[f][c] == 1:
-                    pygame.draw.rect(window, (255, 255, 255), (x, y, TAM, TAM))
-                elif Horlan.matriz[f][c] == 2:
+                if Horlan.matriz[f][c] in Horlan.colores and Horlan.revisor[f][c] != -2:
+                    pygame.draw.rect(window, Horlan.color[Horlan.matriz[f][c]], (x, y, TAM, TAM))
+                if Horlan.matriz[f][c] == -1:
                     pygame.draw.rect(window, (255, 0, 0), (x, y, TAM, TAM))
         Horlan.avanzar()
         pygame.display.update()
@@ -36,4 +34,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-
+    
