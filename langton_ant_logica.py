ce2f93d38a118cf4ef5ce45db8b44e97071e62ab
AlexartG <alexart6122001@gmail.com>
2026-06-03 12:34:09 -0600

    Update langton_ant_logica.py
    Pulido

--- langton_ant_logica.py
+++ langton_ant_logica.py
@@ -1,46 +1,39 @@
 import random
-import Matrices
 
-#Matriz
+#Ingresar string (LR)
+#Ingresar tamaño de filas, columnas y tamaño de la página
+
 matriz = []
 filas = 20
 cols = 20
 
-#Hormiga
 ant_f = 0
 ant_c = 0
-hormiga = 2
-dirección = "U" # U.Up D.Down R.Right L.Left
+hormiga = -1
+dirección = "U"
 
-célula = 0 #Hay que determinar si está viva o muerta
+célula = 0
+colores = []
+color = []
+giro = []
+revisor = []
+L = {}
+R = {}
 
-def siguiente_posición(célula, matriz):
-    if dirección == "R" and célula == 0 or dirección == "L" and célula == 1:
+def siguiente_posición(giro, célula, matriz):
+    if dirección == "R" and giro[célula % len(giro)] == "R" or dirección == "L" and giro[célula % len(giro)] == "L":
         cambiar_dirección("D")
-        if ant_f + 1 == len(matriz):
-            return 0, ant_c
-        else:
-            return ant_f + 1, ant_c
-    elif dirección == "R" and célula == 1 or dirección == "L" and célula == 0:
+        return (ant_f + 1) % len(matriz), ant_c
+    elif dirección == "R" and giro[célula % len(giro)] == "L" or dirección == "L" and giro[célula % len(giro)] == "R":
         cambiar_dirección("U")
-        if ant_f - 1 < 0:
-            return len(matriz) - 1, ant_c
-        else:
-            return ant_f - 1, ant_c
-    elif dirección == "U" and célula == 0 or dirección == "D" and célula == 1:
+        return (ant_f - 1) % len(matriz), ant_c
+    elif dirección == "U" and giro[célula % len(giro)] == "R" or dirección == "D" and giro[célula % len(giro)] == "L":
         cambiar_dirección("R")
-        if ant_c + 1 == len(matriz[0]):
-            return ant_f, 0
-        else:
-            return ant_f, ant_c + 1
-    elif dirección == "U" and célula == 1 or dirección == "D" and célula == 0:
+        return ant_f, (ant_c + 1) % len(matriz[0])
+    elif dirección == "U" and giro[célula % len(giro)] == "L" or dirección == "D" and giro[célula % len(giro)] == "R":
         cambiar_dirección("L")
-        if ant_c - 1 < 0:
-            return ant_f, len(matriz[0]) - 1
-        else:
-            return ant_f, ant_c - 1
+        return ant_f, (ant_c - 1) % len(matriz[0])
 
-#Puede ser que esta subrutina la podamos quitar
 def cambiar_dirección(nueva_dirección):
     global dirección
     if nueva_dirección in ("U", "D") and dirección in ("L", "R"):
@@ -48,28 +41,58 @@ def cambiar_dirección(nueva_dirección):
     elif nueva_dirección in ("L", "R") and dirección in ("U", "D"):
         dirección = nueva_dirección
 
-#Incompleto (más o menos una idea)
 def avanzar():
-    global ant_f, ant_c, matriz, célula
-    nueva_f, nueva_c = siguiente_posición(célula, matriz)
-    if matriz[nueva_f][nueva_c] == 0:
-        matriz[ant_f][ant_c] = 1
-    if matriz[nueva_f][nueva_c] == 1:
-        matriz[ant_f][ant_c] = 0
+    global ant_f, ant_c, matriz, célula, revisor
+    nueva_f, nueva_c = siguiente_posición(giro, célula, matriz)
+    matriz[ant_f][ant_c] = (célula + 1) % len(colores)
     célula = matriz[nueva_f][nueva_c]
     matriz[nueva_f][nueva_c] = hormiga
+    revisor[nueva_f][nueva_c] = célula
     ant_f = nueva_f
     ant_c = nueva_c
 
+def generar_colores():
+    global colores, color, giro
+    string = "LLRLLLRRRRRR"
+    giro = []
+    giro = list(string)
+    colores = []
+    for i in range(len(string)):
+        colores.append(i)
+    color = []
+    for i in range(len(colores)):
+        color.append((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+
+def crear_matriz_random(filas, cols):
+    M = []
+    for f in range(filas):
+        fila = []
+        for c in range(cols):
+            fila.append(random.randint(0, len(colores) - 1))
+        M.append(fila)
+    return M
+
+def crear_matriz(filas, cols, valor):
+    M = []
+    for f in range(filas):
+        fila = []
+        for c in range(cols):
+            fila.append(valor)
+        M.append(fila)
+    return M
+
 def init():
     global filas, cols, ant_f, ant_c
-    global hormiga, dirección, matriz
-    filas = 50
-    cols = 50
-    matriz = Matrices.crear_matriz(filas, cols, 0)
+    global hormiga, dirección, matriz, color, giro, revisor
+    filas = 250
+    cols = 250
+    generar_colores()
+    matriz = crear_matriz(filas, cols, 0)
+    revisor = crear_matriz(filas, cols, -2)
     ant_f = filas // 2
     ant_c = cols // 2
-    hormiga = 2
+    hormiga = -1
     dirección = "U"
     célula = matriz[ant_f][ant_c]
+    revisor[ant_f][ant_c] = célula
     matriz[ant_f][ant_c] = hormiga
